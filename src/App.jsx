b090c8d9693e2271a7906012a9f72c4f419a6dfc
memolop10 <inglopezvargas@gmail.com>
2021-03-08 17:58:00 -0600

    configurando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import {
+  BrowserRouter as Router, 
+  Switch, 
+  Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="container mt-5">
-      <h1>Auth</h1>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        navbar...
+        <Switch>
+          <Route path="/" exact>
+            inicio...
+          </Route>
+          <Route path="/login">
+            logun...
+          </Route>
+          <Route path="/admin">
+            admin...
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
