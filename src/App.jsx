8b2f332748bdf8cdd9dc1a1be858566909a607ad
memolop10 <inglopezvargas@gmail.com>
2021-03-09 18:03:50 -0600

    configuracion de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import {
   BrowserRouter as Router, 
   Switch, 
   Route} from 'react-router-dom'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="container mt-5">
-        navbar...
+        <Navbar/>
         <Switch>
           <Route path="/" exact>
             inicio...
           </Route>
           <Route path="/login">
-            logun...
+            login...
           </Route>
           <Route path="/admin">
             admin...
