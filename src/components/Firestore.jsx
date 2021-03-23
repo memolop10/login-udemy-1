1a92be8e5fd0720a8a1360bfa5664a51848de9f6
memolop10 <inglopezvargas@gmail.com>
2021-03-23 12:23:32 -0600

    uso de moment.js

--- src/components/Firestore.jsx
+++ src/components/Firestore.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {db} from '../firebase'
+import moment from 'moment'
 
 const Firestore = (props) => {
 
@@ -28,7 +29,7 @@ const Firestore = (props) => {
   
       obtenerDatos()
   
-    }, [])
+    }, [props.user.uid])
   
     const agregar = async (e) => {
       e.preventDefault()
@@ -111,7 +112,7 @@ const Firestore = (props) => {
                         {
                         tareas.map(item => (
                             <li className="list-group-item" key={item.id}>
-                            {item.name}
+                            {item.name} - { moment(item.fecha).format('l')}
                             <button 
                                 className="btn btn-danger btn-sm float-right"
                                 onClick={() => eliminar(item.id)}
